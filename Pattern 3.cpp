b60ae2cf89ea7dc30073e592db1fce19f5f3d0ff
Akash B <113533511+Akash-1006@users.noreply.github.com>
2023-01-29 19:33:59 +0530

    Add files via upload

---
+++ Pattern 3.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+	int i,j,k,l=1;
+	printf("Enter the number of rows:");
+	scanf("%d",&k);
+	for (i=1;i<=k;i++)
+	{for (j=1;j<=i;j++)
+	{
+	printf("%d ",l);
+	l++;
+}
+
+	printf("\n");
+}
+		
+	}
+	
